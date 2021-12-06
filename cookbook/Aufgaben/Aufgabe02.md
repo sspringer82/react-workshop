b00af7b51e6032ecf371117687dfa00c70622603
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-12-06 12:06:49 +0100

    Aufgabe2

---
+++ cookbook/Aufgaben/Aufgabe02.md
@@ -0,0 +1,5 @@
+# Aufgabe 2
+
+Halte die Rezepte im State der Komponente vor.
+
+`const [state, setState] = useState<TYPE>(INITIALWERT)`
--- cookbook/src/demo/State.tsx
+++ cookbook/src/demo/State.tsx
@@ -4,6 +4,16 @@ const State = (): ReactElement => {
   console.log('rerender');
   const [state, setState] = useState<string>('initialwert');
 
+  // überschreibt
+  setState('neuer Wert');
+
+  // arbeitet mit dem alten state und produziert den neuen
+  setState((oldState) => {
+    let newState = oldState;
+    newState += '!';
+    return newState;
+  });
+
   // Keine gute Idee, weil Endlosschleife
   // setTimeout(() => {
   //   setState('nicht mehr initial' + Date.now());
