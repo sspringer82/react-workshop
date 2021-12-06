481e8d50aaf14ff1b29a27f74079ffd3d4c61c92
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-12-06 15:53:42 +0100

    even more classes

--- cookbook/src/demo/ClassComponent.tsx
+++ cookbook/src/demo/ClassComponent.tsx
@@ -1,7 +1,21 @@
 import React, { ReactElement } from 'react';
 
 class Component extends React.Component {
+  state = {
+    name: 'Klaus',
+  };
+
+  componentDidMount() {}
+
+  componentDidUpdate() {}
+
+  componentWillUnmount() {}
+
+  handleClick() {
+    this.setState({ name: 'Peter' });
+  }
+
   render(): ReactElement {
-    return <div>Hallo React</div>;
+    return <button onClick={() => this.handleClick()}></button>;
   }
 }
