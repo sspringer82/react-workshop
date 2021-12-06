6597b1f3404546b12f9ccbb0ebfb850aad726138
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-12-06 14:17:49 +0100

    Child Components

--- cookbook/src/App.tsx
+++ cookbook/src/App.tsx
@@ -1,11 +1,12 @@
 import { ReactElement } from 'react';
 import './App.css';
+import Parent from './demo/Children';
 import Interaction from './demo/Interaction';
 import State from './demo/State';
 import RecipeList from './list/RecipeList';
 
 const App = (): ReactElement => {
-  return <RecipeList />;
+  return <Parent />;
 };
 
 export default App;
---
+++ cookbook/src/demo/Children.tsx
@@ -0,0 +1,47 @@
+import { HTMLAttributes, HTMLInputTypeAttribute, ReactElement } from 'react';
+
+const names = ['Tick', 'Trick', 'Track'];
+
+type Props = {
+  name: string;
+};
+
+const Child = ({ name }: Props): ReactElement => {
+  return <li key={name}>{name}</li>;
+};
+
+type InputProps = {
+  label: string;
+  disabled?: boolean;
+};
+const CustomInput = (
+  props: InputProps & HTMLAttributes<HTMLInputElement>,
+): ReactElement => {
+  const inputProps: any = {};
+  if (props.disabled) {
+    inputProps.disabled = true;
+  }
+
+  return (
+    <label>
+      {props.label}
+      <input type="text" {...inputProps} />
+    </label>
+  );
+};
+
+const Parent = (): ReactElement => {
+  return (
+    <>
+      <ul>
+        {names.map((name) => (
+          <Child name={name} />
+        ))}
+      </ul>
+      <CustomInput label="myCustomInput" disabled />
+      <CustomInput label="myCustomInput" />
+    </>
+  );
+};
+
+export default Parent;
