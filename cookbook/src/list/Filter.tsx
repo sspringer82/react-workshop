4917f08e91f5aa8c110ccad1379f4c08f4916c6d
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-12-07 09:13:30 +0100

    clear filter

--- cookbook/src/list/Filter.tsx
+++ cookbook/src/list/Filter.tsx
@@ -6,6 +6,16 @@ type Props = {
 
 const Filter = ({ onFilter }: Props): ReactElement => {
   const inputRef = useRef<HTMLInputElement>(null);
+
+  function handleClear() {
+    // reset filter
+    onFilter('');
+    // reset input
+    if (inputRef.current?.value) {
+      inputRef.current.value = '';
+    }
+  }
+
   return (
     <form
       onSubmit={(event) => {
@@ -19,6 +29,7 @@ const Filter = ({ onFilter }: Props): ReactElement => {
         Suche: <input type="text" ref={inputRef} />
       </label>
       <button type="submit">suchen</button>
+      <button onClick={handleClear}>clear</button>
     </form>
   );
 };
